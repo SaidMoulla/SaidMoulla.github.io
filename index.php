9650b3428a0c182d07b702dc284ff6f2ff4cfbc7
SAID MOULLA <74224806+SaidMoulla@users.noreply.github.com>
2023-04-06 12:58:12 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to my site!</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-wcNyN1Nvnm8rNwblCkVPRMzAKX9opv2KzBxsh+GGcslPx6hx1/rv8nWjUkl7V/j5a6U5qzU8He6I61lV2l2Fg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-j0T/auKudmT+utNvQ2voEqUkGVf/E/Pb0mWKWQO9ViYf+dx6GW0P6ll8WcUyOvCGedvy0I5OckuNHe6Zq7zU+w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  </head>
+
+  <body>
+    <!-- Header -->
+    <header class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <i class="fas fa-code"></i>
+          My Site
+        </a>
+      </div>
+    </header>
+
+    <!-- Body -->
+    <div class="container my-5">
+      <h1>Welcome to my site!</h1>
+      <p>This is a simple example of a web page built with Bootstrap 5 and Font Awesome 5. Feel free to modify it to suit your needs!</p>
+    </div>
+
+    <!-- Footer -->
+    <footer class="bg-light py-3">
+      <div class="container text-center">
+        &copy; 2023 My Site
+      </div>
+    </footer>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.min.js" integrity="sha512-AFrcZJx6p/ntPZhwUcT+T6zSfLfrHJ1Ym+ZWm+tRLq3x4JHm4+QlpgP4o4qMf5O5zE8kQWcJv5Pd+ZIH/wRtA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- Font Awesome JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js" integrity="sha512-Ki/06bE2JeN/NcOOjc8V7gkCw5F5ix5ryE3wy8W2Jz5vHgjMQfRtRi1gk82uCDwHTExH48R6R37gPJzBNpMECg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  </body>
+</html>
